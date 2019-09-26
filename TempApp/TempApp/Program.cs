59a8bb7355e483b365e0338e6135ab464858988b
Jocienė <S7096B@corp1.ad1.seb.net>
2019-09-26 21:09:18 +0300

    GetInRange function added

--- TempApp/TempApp/Program.cs
+++ TempApp/TempApp/Program.cs
@@ -6,24 +6,14 @@ namespace TempApp
     {
         static void Main(string[] args)
         {
-            int number1 = new Random().Next(0, 100);
-            int number2 = new Random().Next(0, 100);
-            int number3 = new Random().Next(0, 100);
+            Console.WriteLine(GetMaxNumber(5,10,3));
 
-            Console.WriteLine(number1,number2,number3);
-            int number1;
-            int number2;
-            int number3;
-
-            Console.WriteLine(GetMaxNumber(5,10,3)); 
-
-
+            Console.WriteLine(GetInRange(5, 1, 10));
         }
 
         static int GetMaxNumber(int number1, int number2, int number3)
         {
-            
-
+           
             if (number1>number2 && number1 > number3)
             {
                return number1;
@@ -36,9 +26,15 @@ namespace TempApp
             {
                 return number3;
             }
-
-           
+        
         }
+
+        static bool GetInRange (int nr1, int from, int to)
+        {
+            return nr1 >= from && nr1<=to;
+            
+        }
+
         
     }
 }
