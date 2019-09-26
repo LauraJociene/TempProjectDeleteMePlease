c9e0d96970f4a6acaf92914e5204710c5a3814f0
Jocienė <S7096B@corp1.ad1.seb.net>
2019-09-26 21:04:04 +0300

    GetMaxNumber function created

--- TempApp/TempApp/Program.cs
+++ TempApp/TempApp/Program.cs
@@ -6,13 +6,38 @@ namespace TempApp
     {
         static void Main(string[] args)
         {
-            /*
             int number1 = new Random().Next(0, 100);
             int number2 = new Random().Next(0, 100);
             int number3 = new Random().Next(0, 100);
 
             Console.WriteLine(number1,number2,number3);
-            */
+            int number1;
+            int number2;
+            int number3;
+
+            Console.WriteLine(GetMaxNumber(5,10,3)); 
+
+
+        }
+
+        static int GetMaxNumber(int number1, int number2, int number3)
+        {
+            
+
+            if (number1>number2 && number1 > number3)
+            {
+               return number1;
+            }
+            else if (number2 > number3)
+            {
+                return number2;
+            }
+            else
+            {
+                return number3;
+            }
+
+           
         }
         
     }
