22b82e4b20f984152f1eb511450cff0fa4999bb6
Jocienė <S7096B@corp1.ad1.seb.net>
2019-09-26 20:53:48 +0300

    Visual studio project created
    Visual studio project created

---
+++ TempApp/TempApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TempApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int number1 = new Random().Next(0, 100);
+            int number2 = new Random().Next(0, 100);
+            int number3 = new Random().Next(0, 100);
+
+            Console.WriteLine(number1,number2,number3);
+            */
+        }
+        
+    }
+}
